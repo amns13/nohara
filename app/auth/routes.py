cb7cb8ffdee69dbe275136f3bb17511384d7468c
slimmsady <slimmsady@gmail.com>
2020-07-25 15:27:57 +0530

    Added register route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,16 +1,42 @@
 from app.auth import bp
 from app import db
-from models import User
-from flask import redirect, render_template, url_for, request
+from app.models import User
+from flask import redirect, render_template, url_for, request, flash
+
+from sqlalchemy import or_
 
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    username = request.form['username']
-    email = request.form['email']
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        error = None
 
-    user = User(username=username, email=email)
-    user.set_password(request.form['password'])
+        if not username:
+            error = "Username is required."
+        elif not email:
+            error = "Email is required."
+        elif not password:
+            error = "Password is required."
+        elif User.query.filter(or_(User.username == username, User.email == email)).first() is not None:
+            error = "Username or Email already exists."
 
-    db.session.add(user)
-    db.session.commit()
+        if error is None:
+            user = User(username=username, email=email)
+            user.set_password(request.form['password'])
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html', title='Register')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
