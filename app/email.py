ef09219253e735569182f9e2e2fe98efc0828fef
slimmsady <slimmsady@gmail.com>
2020-08-05 21:38:34 +0530

    Made email sending asynchrounous

--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
 from threading import Thread
+from flask import current_app
 from flask_mail import Message
 from app import mail
 
@@ -12,4 +13,5 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email,
+            args=(current_app._get_current_object(), msg)).start()
