3afe9ece4c62818ce8593067e80111933e70338c
slimmsady <slimmsady@gmail.com>
2020-08-25 21:20:53 +0530

    modified templates to properly render rtf posts

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class Post(db.Model):
     body  = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    template_id = db.Column(db.String(128))
 
     def __repr__(self):
         return '<Post %r>' % self.title
