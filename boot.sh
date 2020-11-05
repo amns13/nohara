61e6b6f5c7833cdd35b8289904be9c3881138124
slimmsady <slimmsady@gmail.com>
2020-11-05 09:04:25 +0530

    added gunicorn workers and modified Dockerfile to resolve permission issues with image uploads.

--- boot.sh
+++ boot.sh
@@ -8,4 +8,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -b :5000 --access-logfile - --error-logfile - nohara:app
+exec gunicorn -b :5000 -w 3 --access-logfile - --error-logfile - nohara:app
